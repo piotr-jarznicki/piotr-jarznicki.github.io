d4be2b88d6dd107f62c14913ec9e937eb5e98fcb
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-09-02 14:17:18 +0200

    Fix mobile menu

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const menuButton = document.querySelector(".logo-menu__hamburger-icon");
 const menu = document.querySelector(".main-nav");
 menuButton.addEventListener("click", function () {
+  menu.classList.toggle("slide-in-nav");
+  menu.classList.toggle("slide-out-nav");
   if (menu.classList.contains("slide-in-nav")) {
+    menu.classList.remove("slide-out-nav");
+  } else if (menu.classList.contains("slide-out-nav")) {
     menu.classList.remove("slide-in-nav");
-    menu.classList.add("slide-out-nav");
   }
-
-  menu.classList.add("slide-in-nav");
 });
+
 Barba.Pjax.start();
 
 var FadeTransition = Barba.BaseTransition.extend({
