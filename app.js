ea500e41453d0f6e4fcdd43b11ba7185daf99988
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-09-02 19:18:03 +0200

    Add new page transition

--- app.js
+++ app.js
@@ -9,28 +9,3 @@ menuButton.addEventListener("click", function () {
     menu.classList.remove("slide-in-nav");
   }
 });
-
-Barba.Pjax.start();
-
-var FadeTransition = Barba.BaseTransition.extend({
-  start: function () {
-    Promise.all([this.newContainerLoading, this.fadeOut()]).then(
-      this.fadeIn.bind(this)
-    );
-  },
-
-  fadeOut: function () {},
-
-  fadeIn: function () {
-    this.newContainer.classList.add("slide-in");
-    var that = this;
-    this.newContainer.addEventListener("animationend", function () {
-      that.newContainer.classList.remove("slide-in");
-      that.done();
-    });
-  },
-});
-
-Barba.Pjax.getTransition = function () {
-  return FadeTransition;
-};
